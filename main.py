84f9911edf9bee36f3633c6f4ef81d5ab3668868
Boris Veis <boris.veis@gmail.com>
2023-07-19 10:23:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import pandas as pd
 #start application
 filename='data/SPY.csv'
 names=['date','open','high','low,Adjclose','volume']
-df=pd.read_csv(filename,names=names).tail(-2).dropna(inplace=False)
-df = pd.read_csv(filename, names=names)
+df=pd.read_csv(filename,names=names).tail(-1).dropna(inplace=True)
+df = pd.read_csv(filename, names=names).tail(-1)
 names = ['date', 'open', 'high', 'low,Adjclose', 'volume']
 #print column names
 column_headers = list(df.columns.values)
 print("The Column Header :", column_headers)
 column='volume'
 volmin=(df[column].min())
-volmax=(df[column].dropna()
-//print(volmin)
+volmax=(df[column].max())
+print("volmin:"+volmin)
 print("volmax:"+ volmax)
