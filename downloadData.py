3e303e1e16adba76c280de9f8bc1889d5613a7ec
Boris Veis <boris.veis@gmail.com>
2024-03-19 10:53:52 -0700

    download as single file

---
+++ downloadData.py
@@ -0,0 +1,23 @@
+import time
+
+import yfinance as yf
+from datetime import datetime, timedelta
+import json
+configfilepath="config/config.json"
+with open(configfilepath, 'r') as f:
+    data = json.load(f)
+tickers =data["tickers"]
+targetdir=data["downloadtargetdir"]
+
+timelength=int(data["timelength"])
+# calculate date
+current_date = datetime.now()
+startdate=current_date-timedelta(days=365*timelength)
+for ticker in tickers:
+    time.sleep((.25))
+    print("downloading "+ticker)
+    data=yf.download(tickers, startdate, current_date)
+    targetfile = targetdir+"tickersdatefile"+".csv"
+data.to_csv(targetfile)
+
+    # data.to_csv(targetfile)
